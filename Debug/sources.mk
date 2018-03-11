9a17a255474291d881de08cf247160c22ce06ade
SystemGlitch <jeremy.la@outlook.fr>
2018-03-11 12:27:17 +0100

    Updated gitignore to include makefiles

---
+++ Debug/sources.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src \
+src/gui \
+src/libs/b64 \
+src/libs/muttx \
+
